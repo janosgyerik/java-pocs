09fcb7517e992354e04b8a4cce0d4a9a95cac053
Janos Gyerik <info+kronos@janosgyerik.com>
2019-09-11 18:57:15 +0200

    Add POC of okhttp keeping the process alive

---
+++ okhttp-no-timely-shutdown/src/main/java/poc/okhttp3/App.java
@@ -0,0 +1,41 @@
+package poc.okhttp3;
+
+import java.io.IOException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class App {
+
+  private static final String URL = "https://google.com";
+
+  private final OkHttpClient client = new OkHttpClient.Builder().build();
+
+  private void log(String message) {
+    System.out.println(message);
+  }
+
+  private String get(String url) throws IOException {
+    Request request = new Request.Builder()
+      .url(url)
+      .get()
+      .build();
+    try (Response response = client.newCall(request).execute()) {
+      if (response.isSuccessful()) {
+        return response.body().string();
+      }
+
+      log(String.format("Request failed with https status: %s", response.code()));
+      return null;
+    }
+  }
+
+  private void main() throws IOException {
+    log("Received content: " + get(URL));
+    log("Time to shut down... But it won't just happen, will it?");
+  }
+
+  public static void main(String[] args) throws IOException {
+    new App().main();
+  }
+}
